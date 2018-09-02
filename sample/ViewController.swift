531f3cea7c697f5fb55df4d10acb67bf2492ce97
dai-chan <daijiro.ogura@tabi-labo.com>
2018-09-02 18:33:34 +0900

    Initial Commit

---
+++ sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by 小倉大二郎 on 2018/09/02.
+//  Copyright © 2018年 dogura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
